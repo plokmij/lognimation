4e31ec00fe6e1a740b2fe1bec5443e760c077118
plokmij <samfan@live.com>
2019-05-27 00:40:31 +0530

    first step finished

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,14 +1,77 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  Widget build(BuildContext context){
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  AnimationController loginController;
+  Animation buttonShrink;
+
+  void initState() {
+    super.initState();
+    loginController = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    );
+    buttonShrink = Tween(
+      begin: 320.0,
+      end: 70.0,
+    ).animate(
+      CurvedAnimation(
+        parent: loginController,
+        curve: Curves.linear,
+      ),
+    );
+    //loginController.forward();
+  }
+
+  Widget build(BuildContext context) {
     return Center(
-      child: Text("Myreeey"),
+      child: Scaffold(
+        body: Center(
+          child: buildButton(),
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton() {
+    return InkWell(
+      onTap: () {
+        if (loginController.status == AnimationStatus.completed) {
+          loginController.reverse();
+        } else if (loginController.status == AnimationStatus.dismissed) {
+          loginController.forward();
+        }
+      },
+      child: new AnimatedBuilder(
+        animation: buttonShrink,
+        //child: ,
+        builder: (context, child) {
+          return Container(
+            width: buttonShrink.value,
+            height: 60.0,
+            alignment: FractionalOffset.center,
+            decoration: new BoxDecoration(
+              color: const Color.fromRGBO(247, 64, 106, 1.0),
+              borderRadius: new BorderRadius.all(const Radius.circular(30.0)),
+            ),
+            child: buttonShrink.value > 80
+                ? Text(
+                    "Hello",
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: 0.3,
+                    ),
+                  )
+                : CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
